0d9dc8319cd8d17ba6084b4b980285ebb892763d
NIMMY04 <156294603+NIMMY04@users.noreply.github.com>
2024-09-28 16:38:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function insert_Row() {
-    //Write your code here
-  
+ var table = document.getElementById("sampleTable");
+
+	var newRow = table.insertRow(1);
+	var cell1 = newRow.insertcell(0);
+	cell1.innerHTML = "New cell1";
+	
+	var cell2 = newRow.insertcell(1);
+    cell2.innerHTML = "New cell2";
   
 }
